60b9115424769cebd709e5a41541ecc51c566400
Michael Bayer <mab4058@gmail.com>
2019-04-26 17:51:21 -0400

    Initial commit.

---
+++ appbundler/appbundler.py
@@ -0,0 +1,144 @@
+import functools
+import logging
+import os
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+import toml
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+logger.setLevel(logging.DEBUG)
+
+
+def log_entrance_exit(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        logger.info('Entering %s.', func.__name__)
+        results = func(*args, **kwargs)
+        logger.info('Exiting %s.', func.__name__)
+        return results
+
+    return wrapper
+
+
+class cd:
+    """Context manager for temporary cd."""
+
+    def __init__(self, destiation):
+        self.original_dir = os.getcwd()
+        self.new_dir = destiation
+
+    def __enter__(self):
+        os.chdir(self.new_dir)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir(self.original_dir)
+
+
+class Config:
+    def __init__(self, config_file):
+        self.config = toml.load(config_file)
+
+    @property
+    def package(self):
+        return self.config['package']
+
+    @property
+    def data(self):
+        return self._parse_and_verify(self.config['data'])
+
+    @staticmethod
+    def _parse_and_verify(data):
+        """
+        Parses the paths and verifies their existence.
+
+        Args:
+            data (dict): Dictionary containing paths as values.
+
+        Returns:
+            dict: Dictionary with the same keys, but Path instances as values.
+
+        Raises:
+            ValueError: If path does not exist.
+
+        """
+
+        new = {}
+        for k, v in data.items():
+            path = Path(v)
+            if not path.exists():
+                raise ValueError('Path does not exist: %s', v)
+            else:
+                new[k] = path.resolve()
+        return new
+
+
+class AppBundler:
+    """
+    Handles bundling all dependencies and supplemental data into a nice zip file.
+
+    All path must be absolute or relative to app_directory.
+    """
+
+    def __init__(self, app_directory, package_name=None, supplemental_data=None, build_directory=None):
+        self.app_directory = Path(app_directory).resolve()
+        self.package_name = package_name
+        self.supplemental_data = supplemental_data
+        self.build_directory = build_directory
+
+        # Compute paths.
+        if package_name is None:
+            self.package = None
+        else:
+            self.package = self.app_directory.joinpath(package_name).resolve()
+
+        if build_directory is None:
+            self.build_directory = self.app_directory.joinpath('build')
+        else:
+            self.build_directory = Path(build_directory).joinpath('build').resolve()
+
+        try:
+            self.build_directory.mkdir(parents=True)
+        except FileExistsError:
+            shutil.rmtree(self.build_directory)
+            self.build_directory.mkdir(parents=True)
+
+        self._install_dependencies()
+        self._cleanup_files()
+
+    @log_entrance_exit
+    def _install_dependencies(self):
+        """Installs dependencies contained in requirements.txt or setup.py."""
+
+        requirements_file = self.app_directory.joinpath('requirements.txt')
+        setup_file = self.app_directory.joinpath('setup.py')
+
+        if requirements_file.exists():
+            cmd = [sys.executable, '-m', 'pip', 'install', '-r', str(requirements_file), '-t',
+                   str(self.build_directory)]
+            logger.debug('Running subprocess cmds: %s', cmd)
+            _ = subprocess.run(cmd, check=True)
+            shutil.copytree(self.package_name, self.build_directory)
+        elif setup_file.exists():
+            cmd = [sys.executable, '-m', 'pip', 'install', str(setup_file.parent), '-t', str(self.build_directory)]
+            logger.debug('Running subprocess cmds: %s', cmd)
+            _ = subprocess.run(cmd, check=True)
+        else:
+            raise ValueError('Could not locate requirements.txt or setup.py.')
+
+    @log_entrance_exit
+    def _cleanup_files(self):
+        """Removes __pycache__ and .pyc files resulting from installation."""
+
+        for root, dirs, files in os.walk(self.build_directory):
+            dirs_to_delete = [Path(root).joinpath(x) for x in dirs if x == '__pycache__']
+            files_to_delete = [Path(root).joinpath(x) for x in files if Path(x).suffix == '.pyc']
+            for d in dirs_to_delete:
+                logger.info('Deleting: %s', d)
+                shutil.rmtree(d)
+            for f in files_to_delete:
+                logger.info('Deleting: %s', f)
+                f.unlink()
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages
+from setuptools import setup
+
+version = '0.0.0'
+
+setup(name='appbundler',
+      version=version,
+      description='Bundle an app, dependencies, and any other files into a neat zip file.',
+      author='Michael Bayer',
+      author_email='mab4058@gmail.com',
+      url='https://github.com/mab4058/appbundler',
+      download_url='https://github.com/mab4058/appbundler/appbundler/archive/{}.tar.gz'.format(version),
+      license='MIT',
+      install_requires=['toml>=0.10.0,<1.0.0'],
+      extras_require={
+          'tests': ['pytest>=3.5.0,<4.0.0'],
+      },
+      python_requires='>=3.6',
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3 :: Only',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
