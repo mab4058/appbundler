c3d0779eac0abec42fc5e4057df4482814c070e6
Michael Bayer <mab4058@gmail.com>
2019-05-04 09:41:59 -0400

    Adding get_version function to setup.py to parser version str from __init__

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
+import ast
+import re
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.0.0'
+CURRENT_DIR = Path(__file__).parent
+
+
+def get_version():
+    """
+    Gets the version from __init__.
+
+    Credit:
+        This function was adapted from python/black on Github.
+
+    Returns:
+        str: Version string.
+
+    """
+    init_file = CURRENT_DIR.joinpath('appbundler/__init__.py')
+    _version_re = re.compile(r"__version__\s+=\s+(?P<version>.*)")
+    with open(init_file, "r", encoding="utf8") as f:
+        match = _version_re.search(f.read())
+        version = match.group("version") if match is not None else '"unknown"'
+    return str(ast.literal_eval(version))
+
 
 setup(name='appbundler',
-      version=version,
+      version=get_version(),
       description='Bundle an app, dependencies, and any other files into a neat zip file.',
       author='Michael Bayer',
       author_email='mab4058@gmail.com',
