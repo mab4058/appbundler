9d2438ce018dd4b3ffe4b83016195967402294e6
Michael Bayer <mab4058@gmail.com>
2019-05-06 13:28:01 -0400

    Formatting.

--- appbundler/__main__.py
+++ appbundler/__main__.py
@@ -13,36 +13,38 @@ logger = logging.getLogger()
 
 def main():
     parser = argparse.ArgumentParser(description='Bundle an app.')
-    parser.add_argument('config',
-                        help='Full path to an appBuilder config file. This '
-                             'must be located in the app\'s root directory.')
-    parser.add_argument('--dir',
-                        required=False,
-                        type=str,
-                        help='Override the default built directory.')
+    parser.add_argument(
+        'config',
+        help='Full path to an appBuilder config file. This '
+        'must be located in the app\'s root directory.',
+    )
+    parser.add_argument(
+        '--dir', required=False, type=str, help='Override the default built directory.'
+    )
     flags = parser.add_argument_group('Flags')
-    flags.add_argument('--zip',
-                       action='store_true',
-                       default=False,
-                       required=False,
-                       help='Create zip file of the build.')
-    flags.add_argument('-v',
-                       action='count',
-                       required=False,
-                       help='Add a logger handler to stdout. \'v\' for INFO,'
-                            '\'vv\' for DEBUG')
+    flags.add_argument(
+        '--zip',
+        action='store_true',
+        default=False,
+        required=False,
+        help='Create zip file of the build.',
+    )
+    flags.add_argument(
+        '-v',
+        action='count',
+        required=False,
+        help='Add a logger handler to stdout. \'v\' for INFO,' '\'vv\' for DEBUG',
+    )
 
     args = parser.parse_args()
 
-    log_levels = {
-        1: logging.INFO,
-        2: logging.DEBUG
-    }
+    log_levels = {1: logging.INFO, 2: logging.DEBUG}
 
     if args.v:
         handler = logging.StreamHandler(stream=sys.stdout)
         formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
         logger.setLevel(log_levels[args.v])
@@ -62,7 +64,7 @@ def main():
         config.package,
         supplemental_data=config.data,
         build_directory=args.dir,
-        make_zip=args.zip
+        make_zip=args.zip,
     )
     bundler.bundle()
 
