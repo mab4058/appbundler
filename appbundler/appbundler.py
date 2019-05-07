8f47c339da8e93dd64d4bddea2437543241af6fc
Michael Bayer <mab4058@gmail.com>
2019-05-07 15:51:29 -0400

    remove newline.

--- appbundler/appbundler.py
+++ appbundler/appbundler.py
@@ -17,8 +17,7 @@ class Config:
     """
     Loads the appbundler.toml file and handles some initial setup.
 
-    All relative paths are relative to the
-    appbundler.toml file.
+    All relative paths are relative to the appbundler.toml file.
 
     Args:
         config_file (str, pathlib.Path): Path to the appbuilder.toml config file.
